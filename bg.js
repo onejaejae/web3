f594ab572d672b187893c1d5fad037b7ce6f3810
onejaejae <62149784+onejaejae@users.noreply.github.com>
2020-04-13 19:32:17 +0900

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body");
+const IMAGE_COUNT =8;
+
+function paintBg(num){
+    const image = new Image();
+    image.src = `image/${num+1}.jpg`;
+    image.classList.add("bg");  
+    body.appendChild(image);
+}
+
+function genRandom(){
+    const random = Math.floor(Math.random()* IMAGE_COUNT);
+    return random;
+}
+function init(){
+    const randomNum = genRandom();
+    paintBg(randomNum);
+}
+init();
